d8853813d34fe7c2a9d460f11368d582797cfd8b
Adelmo Gama <gama.a.jose@gmail.com>
2025-09-18 07:50:42 -0300

    MaTeste do seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_categoria (descricao) VALUES('Curso');
+INSERT INTO tb_categoria (descricao) VALUES('Oficina');
+
+INSERT INTO tb_bloco (inicio, fim) VALUES('2017-09-25T08:00:00', '2017-09-25T11:00:00');
+INSERT INTO tb_bloco (inicio, fim) VALUES('2017-09-25T14:00:00', '2017-09-25T18:00:00');
+INSERT INTO tb_bloco (inicio, fim) VALUES('2017-09-26T08:00:00', '2017-09-26T11:00:00');
+
+INSERT INTO tb_participante (nome, email) VALUES('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Teresa Silva', 'teresa@gmail.com');
